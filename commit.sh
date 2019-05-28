fe29d61af5951d53dc3c801181bfb7a9029d8394
Omkar Kakade <omkar.kakade@gmail.com>
2019-05-28 17:08:16 -0400

    commit script via the script itself

--- commit.sh
+++ commit.sh
@@ -2,9 +2,9 @@
 #author : Omkar Kakade
 #date : 28 May 2019
 #Change to repo as per the specified REPO_PATH, add a commit message provided in the command line and push to the repo.
-
-export $REPO_PATH
+export REPO_PATH
 cd $REPO_PATH
+echo $REPO_PATH
 git add .
 git commit -m "$1"
 git push
