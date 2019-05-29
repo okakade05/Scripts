f75a70629facd3b9d758c1fb9e83e00396b618c7
Omkar Kakade <omkar.kakade@gmail.com>
2019-05-29 17:14:15 -0400

    comments

--- commit.sh
+++ commit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #author : Omkar Kakade
 #date : 28 May 2019
-#Change to repo as per the specified REPO_PATH, add a commit message provided in the command line and push to the repo.
+#Change to repo as per the specified variable via the command line, add a commit message provided in the command line and push to the repo.
 cd $1
-echo $REPO_PATH
+echo $1
 git add .
 git commit -m "$2"
 git push
